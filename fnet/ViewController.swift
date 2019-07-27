b53ce6e0bee0c30ae721f9e9a1e484d48761f892
Ahmed Abdelmagied <abdelmagied@Ahmeds-Mac.local>
2019-07-27 01:45:32 -0700

    Initial Commit

---
+++ fnet/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  fnet
+//
+//  Created by Ahmed Abdelmagied on 7/27/19.
+//  Copyright © 2019 Ahmed Abdelmagied. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
